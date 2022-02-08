5c192d401c28a42ced757839d3f2de6df9debfff
krishna-s11 <krishnasaxena69@gmail.com>
2022-02-08 23:52:55 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,27 +2,57 @@ import "./App.css";
 import LeftPane from "./Components/LeftPane/LeftPane";
 import Nav from "./Components/Nav/Nav";
 import Dashboard from "./Pages/Dashboard/Dashboard";
-import Student_Management from "./Pages/StudentManagement/Student_Management";
+import StudentManagement from "./Pages/StudentManagement/StudentManagement";
 import Application from "./Pages/Application/Application";
 import AboutUs from "./Pages/AboutUs/AboutUs";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <LeftPane />
       <div className="main_container">
-      <Nav 
-        title="Application"
-      />
-        {/* <Student_Management/>  */}
-        {/* <Application/>  */}
-        <AboutUs/>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Nav title="Dashboard" />
+                <Dashboard />
+              </>
+            }
+          />
+          <Route
+            exact
+            path="/students"
+            element={
+              <>
+                <Nav title="Student Management" />
+                <StudentManagement />
+              </>
+            }
+          />
+          <Route
+            exact
+            path="/application"
+            element={
+              <>
+                <Nav title="Application" />
+                <Application />
+              </>
+            }
+          />
+          <Route
+            exact
+            path="/about-us"
+            element={
+              <>
+                <Nav title="About Us" />
+                <AboutUs />
+              </>
+            }
+          />
+        </Routes>
       </div>
       <div className="bottom-theme"></div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
